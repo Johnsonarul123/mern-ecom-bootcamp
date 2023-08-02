24ef1ad308268650cdfda0cadb09469ac84ec221
[Arul Johnson] <[aruljohnson9411@gmail.com]>
2023-08-02 18:36:50 +0530

    added stipe checkout

--- attachment_08_Order_helpers_lyst5258/src/core/Cart.js
+++ attachment_08_Order_helpers_lyst5258/src/core/Cart.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "../styles.css";
-import { API } from "../backend";
+// import { API } from "../backend";
 import Base from "./Base";
 import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
+import StripeCheckout from "./StripeCheckout";
+
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
@@ -42,7 +44,8 @@ const Cart = () => {
     <Base title="Cart Page" description="Ready to checkout">
       <div className="row text-center">
         <div className="col-6">{loadAllProducts()}</div>
-        <div className="col-6">Checkout section</div>
+        <div className="col-6"><StripeCheckout products={products} setReload={setReload} />
+        </div>
       </div>
     </Base>
   );
